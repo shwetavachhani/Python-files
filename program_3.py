e0dcd13c0d700bd894c1d911b37367e939accede
Shweta Rangani <40855339+shwetavachhani@users.noreply.github.com>
2024-06-04 15:53:08 -0230

    program in Python to check if a sequence is a Palindrome.

--- program_3.py
+++ program_3.py
@@ -1,6 +1,7 @@
 #check if a sequence is a Palindrome.
 
 def isPalindrome(s):
+    print('hi')
     return s == s[::-1]
   
 # Driver code
@@ -11,3 +12,15 @@ if ans:
     print("Yes")
 else:
     print("No")
+
+#Python palindrome program w/o string functions
+def reverse(text):
+    if len(text) <= 1:
+        return text
+    return reverse(text[1:]) + text[0]
+
+n = input("ENTER  STRING\n")
+if n==reverse(n):
+    print ("It's a palindrome")
+else:
+    print ("It's not a palindrome")
