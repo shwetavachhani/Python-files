bd22bafa6cab87695332f51fb87f525b14d326df
Shweta Rangani <40855339+shwetavachhani@users.noreply.github.com>
2024-06-05 21:25:16 -0230

    Check if a Python string contains another string.

---
+++ program_7.py
@@ -0,0 +1,10 @@
+# Check if a Python string contains another string.
+
+
+# Take input from users 
+MyString = "Earth is an incredible planet, teeming with diverse life forms and natural beauty."
+  
+if "incredible" in MyString: 
+    print("Yes! it is present in the string") 
+else: 
+    print("No! it is not present") 
