8e372e807f4b6107c8010458e0905fbdc8bf8ede
Shweta Rangani <40855339+shwetavachhani@users.noreply.github.com>
2024-07-01 17:52:29 -0230

    Program to hollow rectangle

--- program_11.py
+++ program_11.py
@@ -1,3 +1,20 @@
 # Program to hollow rectangle
 
 
+def hollow_rect(n,m):
+    for i in range(1,n):
+        for j in range(1,m):
+            if(i==1 or i==n or j==1 or j==m):
+                print("*",end="")
+            else:
+                print(" ",end="")
+
+
+
+
+rows=6
+cols=20
+hollow_rect(rows,cols)
+
+
+
